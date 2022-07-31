c3c50519f4a11ea25728a67405e49cde482d9c21
Jovin <jovingo1234@gmail.com>
2022-07-31 01:18:24 -0700

    Completed Day 1 of 30DaysofJS. Day 2 nearly done

---
+++ Day-1-Introduction/introduction.js
@@ -0,0 +1,44 @@
+// console.log('Welcome to The GoLoco Show')
+// console.log(typeof 'Jonathan') // string
+// console.log(typeof 30) // number
+// console.log(typeof true) // boolean
+// console.log(typeof null) // object type
+// console.log(typeof undefined) // undefined
+
+/*
+    Rules of JS var names:
+        should not begin with a variable
+        does not allow for special characters except $ and _
+        JS var follows camelCase convnetion (CamelCase for classes)
+        no spaces between words
+*/
+
+//Sytax
+    // let nameOfVariable = value
+
+//Declare strings
+// let firstName = 'Jovin'
+// let lastName = 'Go-Oco'
+// let country = "Murica"
+// let city = 'SF'
+// let age = 24
+// let isMarried = false
+
+// console.log(firstName, lastName, country, city, age, isMarried)
+
+// //Declarenums
+// const gravity = 9.81
+// const boilingPoint = 100
+// const PI = 3.14
+// console.log(gravity, boilingPoint, PI)
+
+// let name = 'Jovin', job = 'Unemployed'
+// console.log(name, job)
+
+//Day 1: Exercises
+//comments make code readable
+//Welcome to 30DaysOfJavaScript
+/*
+    comments can make code readable,
+    easy to use and informative
+*/
---
+++ Day-1-Introduction/variable.js
@@ -0,0 +1,25 @@
+let whereIAmStreaming = 'YouTube'
+let isYoutubeCool = true
+let howAreYou 
+let howManyDonations = null
+// console.log(whereIAmStreaming, isYoutubeCool, howAreYou, howManyDonations)
+
+// //we did datatypes.js here
+// console.log(typeof whereIAmStreaming, typeof isYoutubeCool, typeof howAreYou, typeof howManyDonations)
+
+
+let time
+let money
+
+// let firstName = 'Jovin'
+// let lastName = 'Go-Oco'
+// let martialStatus = 'single'
+// let country = 'Murica'
+// let age = 24
+
+let firstName = 'Jovin',  lastName = 'Go-Oco', martialStatus = 'single' , country = 'Murica', age = 24
+
+let myAge = 24
+let yourAge = 30
+console.log ("I am", myAge, 'years old.')
+console.log ('You are', yourAge, 'years old.')
